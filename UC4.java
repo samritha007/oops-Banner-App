1a3dea339031b7f296e67047882600ffba31bb12
samritha007 <sam78892007@gmail.com>
2026-03-11 08:38:47 +0530

    UC4.java

--- UC4.java
+++ UC4.java
@@ -1,15 +1,19 @@
-public class UC4 {
+public class OOPSBannerApp {
     public static void main(String[] args) {
+
+        // 1. Define a String array with 7 lines
         String[] lines = new String[7];
 
-        lines[0] = String.join(" ", "   ***   ", "***   ", "******  ", "     *****");
-        lines[1] = String.join(" ", "  **  ** ", "**  **", "**    ", "    **    ");
-        lines[2] = String.join(" ", " **    **", "**  **", "**    ", "    **    ");
-        lines[3] = String.join(" ", " **    **", "**  **", " ***** ", "       ***");
-        lines[4] = String.join(" ", " **    **", "**  **", "     **", "        **");
-        lines[5] = String.join(" ", "  **  ** ", "**  **", "     **", "        **");
-        lines[6] = String.join(" ", "   ***   ", "***   ", "**    ", "     *****");
+        // 2. Populate the array using String.join()
+        lines[0] = String.join(" ", " *** ", " *** ", " ***** ", " ***** ");
+        lines[1] = String.join(" ", "** **", "** **", "** ** ", "**    ");
+        lines[2] = String.join(" ", "** **", "** **", "** ** ", "**    ");
+        lines[3] = String.join(" ", "** **", "** **", "***** ", " ***  ");
+        lines[4] = String.join(" ", "** **", "** **", "**    ", "   ** ");
+        lines[5] = String.join(" ", "** **", "** **", "**    ", "   ** ");
+        lines[6] = String.join(" ", " *** ", " *** ", "**    ", "***** ");
 
+        // 3. Use a for-each loop to print each line
         for (String line : lines) {
             System.out.println(line);
         }
